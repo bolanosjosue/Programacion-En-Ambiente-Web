5c5702fbb14bd2a8a3465295a3d3db660ac93046
bolanosjosue <josuebolanos2004@mail.com>
2023-09-16 18:52:34 -0600

    Programa reloj

---
+++ Workshop 1/Reloj/fecha.php
@@ -0,0 +1,4 @@
+<?php
+  $fecha_hora_actual = date("d-m-Y H:i:s");
+  echo $fecha_hora_actual;
+?>
---
+++ Workshop 1/Reloj/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Fecha y hora actuales</title>
+</head>
+<body>
+  <h1>Fecha y hora actuales</h1>
+
+  <p>La fecha y hora actuales son:</p>
+
+  <?php
+  include("fecha.php");
+  ?>
+
+</body>
+</html>
